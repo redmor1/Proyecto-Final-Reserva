4b96e10d738def5b2a610733af03d99a498c953c
redmor <lightz17@hotmail.com>
2022-12-16 07:47:50 -0300

    Agregar modelo de Post

--- reserva/apps/noticias/models.py
+++ reserva/apps/noticias/models.py
@@ -3,3 +3,17 @@ from django.db import models
 # Create your models here.
 
 
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100, blank=False, null=False)
+    imagen = models.ImageField(upload_to='post', null=True)
+    resumen = models.TextField(max_length=350, blank=False, null=False)
+    texto = models.TextField(max_length=None, blank=False, null=False)
+    fecha_creacion = models.DateField(auto_now_add=True)
+    comentarios_activados = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = 'Posts'
+
+    def __str__(self):
+        return str(self.titulo)
