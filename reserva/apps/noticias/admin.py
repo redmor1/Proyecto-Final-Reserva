9f9c33a80502d3b0955ffe4617345e739315409a
redmor <lightz17@hotmail.com>
2022-12-16 08:52:16 -0300

    Registrar modelo de Categoria en admin

--- reserva/apps/noticias/admin.py
+++ reserva/apps/noticias/admin.py
@@ -8,10 +8,18 @@ class PostAdmin(admin.ModelAdmin):
     ordering = ('id', 'titulo', 'fecha_creacion', 'comentarios_activados')
     search_fields = ('id', 'titulo', 'fecha_creacion', 'resumen', 'texto')
     list_display = ('id', 'titulo', 'fecha_creacion', 'comentarios_activados')
-    list_filter = ('fecha_creacion', 'comentarios_activados')
+    list_filter = ('categoria', 'fecha_creacion', 'comentarios_activados')
+
+class CategoriaAdmin(admin.ModelAdmin):
+    ordering = ('id', 'nombre', 'activado', 'fecha_creacion')
+    search_fields = ('id', 'nombre', 'activado', 'fecha_creacion')
+    list_display = ('id', 'nombre', 'activado', 'fecha_creacion')
+    list_filter = ('activado',)
 
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Categoria, CategoriaAdmin)
+
 
 
 
