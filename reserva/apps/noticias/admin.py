a3095653281fe2fcb886aca17367b3af2be23cb3
redmor <lightz17@hotmail.com>
2022-12-16 07:48:19 -0300

    Registrar modelo de Post en pagina de Admin

--- reserva/apps/noticias/admin.py
+++ reserva/apps/noticias/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from apps.noticias.models import *
+from .models import *
 
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    ordering = ('id', 'titulo', 'fecha_creacion', 'comentarios_activados')
+    search_fields = ('id', 'titulo', 'fecha_creacion', 'resumen', 'texto')
+    list_display = ('id', 'titulo', 'fecha_creacion', 'comentarios_activados')
+    list_filter = ('fecha_creacion', 'comentarios_activados')
+
+
+admin.site.register(Post, PostAdmin)
+
 
 
    
